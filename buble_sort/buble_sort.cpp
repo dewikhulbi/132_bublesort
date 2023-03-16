c91d7beaa160b15a48ce2fb114a61087ffc4f0cc
dewikhulbi <dewikhlb20@gmail.com>
2023-03-16 15:59:43 +0700

    procedure bubble sort array

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -15,13 +15,32 @@ void input() {				//procedur untuk input
 			cout << "\nArray dapat mempunyai 20 elemen.\n";	//output ke layar
 		}
 	}
-	cout << end1;								//output baris kosong
-	cout << "===================" << end1;		//output ke layar
-	cout << "Masukan Elemen Array" << end1;		//output ke layar
-	cout << "===================" << end1;		//output ke layar
+	cout << endl;								//output baris kosong
+	cout << "===================" << endl;		//output ke layar
+	cout << "Masukan Elemen Array" << endl;		//output ke layar
+	cout << "===================" << endl;		//output ke layar
 
 	for (int i = 0; i < n; i++) {	//Looping dengan i dimulai dari 0 hingga n-1
 		cout << "Data ke-" << (i + 1) << ":";	//output ke layar
-		cin >> A[i];				//Input dari pengguna
+		cin >> a[i];				//Input dari pengguna
 	}
+}
+void bubbleSortArray() {			//procedur untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {			//Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - 1; j++) {	//Looping dengan j dimulai dari 0 hingga n-i-1
+			if (a[j] > a[j + 1]) {			//Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];			//simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];			//assign nilai a[j+1] ke a[j]
+				a[j + 1 = temp; ]			//assign nilai temp ke a[j+1]
+			}
+		}
+	}
+}
+
+
+
+
+int main()
+{
+	std:: << "Hello World!\n";
 }
