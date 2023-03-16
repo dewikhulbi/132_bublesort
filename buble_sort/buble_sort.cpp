57dd7f3e48fd8ff426a9ab5306521b1eb913b4e9
dewikhulbi <dewikhlb20@gmail.com>
2023-03-16 16:43:16 +0700

    fix problem

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -27,11 +27,11 @@ void input() {				//procedur untuk input
 }
 void bubbleSortArray() {			//procedur untuk mengurutkan array dengan metode bubble sort
 	for (int i = 1; i < n; i++) {			//Looping dengan i dimulai dari 1 hingga n-1
-		for (int j = 0; j < n - 1; j++) {	//Looping dengan j dimulai dari 0 hingga n-i-1
+		for (int j = 0; j < n - i; j++) {	//Looping dengan j dimulai dari 0 hingga n-i-1
 			if (a[j] > a[j + 1]) {			//Jika nilai pada a[j] lebih besar dari a[j+1]
 				int temp = a[j];			//simpan nilai a[j] ke variabel sementara temp
 				a[j] = a[j + 1];			//assign nilai a[j+1] ke a[j]
-				a[j + 1 = temp; ]			//assign nilai temp ke a[j+1]
+				a[j + 1 ] = temp;		//assign nilai temp ke a[j+1]
 			}
 		}
 	}
@@ -42,7 +42,7 @@ void unsorted() {
 	cout << "===================" << endl;		//output ke layar
 	cout << "Masukan Elemen Array" << endl;		//output ke layar
 	cout << "===================" << endl;		//output ke layar
-	for (int j = 1; j < n; j++) {			//Looping dengan j dimulai dari 1 hingga n-1
+	for (int j = 0; j < n; j++) {			//Looping dengan j dimulai dari 1 hingga n-1
 		cout << a[j] << endl;				//output ke layar
 	}
 	cout << endl;
@@ -53,7 +53,7 @@ void display() {								//procedur untuk menampilkan hasil
 	cout << "===================" << endl;		//output ke layar
 	cout << "Masukan Elemen Array" << endl;		//output ke layar
 	cout << "===================" << endl;		//output ke layar
-	for (int j = 1; j < n; j++) {			//Looping dengan j dimulai dari 1 hingga n-1
+	for (int j = 0; j < n; j++) {			//Looping dengan j dimulai dari 1 hingga n-1
 		cout << a[j] << endl;				//output ke layar
 	}
 	cout << endl;
@@ -70,7 +70,3 @@ int main() {
 
 
 
-int main()
-{
-	std:: << "Hello World!\n";
-}
